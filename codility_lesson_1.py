07dde69aa383de55ac42185db3015e52bcc2f0ee
Park-jeong-seop <twinpapa2003@gmail.com>
2018-06-19 17:51:33 +0900

    Create codility_lesson_1.py

---
+++ codility_lesson_1.py
@@ -0,0 +1,19 @@
+def solution(N):
+    # write your code in Python 3.6
+    count = 0
+    real_count = 0
+    flag = False
+    while N > 0:
+        if flag is True:
+            if N % 2 == 0:
+                count += 1
+            else:
+                if real_count <= count:
+                    real_count = count
+                count = 0
+        else:
+            if N % 2 == 1:
+                flag = True
+        N = N // 2
+
+    return real_count
