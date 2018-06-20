edd333a5639bd2fb7907f9f397750dff485e8966
Park-jeong-seop <twinpapa2003@gmail.com>
2018-06-20 11:10:28 +0900

    Create codility_lesson_3-2.py

---
+++ codility_lesson_3-2.py
@@ -0,0 +1,16 @@
+"""
+A = List
+A = [2, 3, 1, 5]
+res = 4
+"""
+def solution(A):
+    length = len(A)+2
+    arr = [0]*length
+    count = 0
+    for item_A in A:
+        arr[item_A] = 1
+
+    for item_arr in arr[1:]:
+        count += 1
+        if item_arr == 0:
+            return count
