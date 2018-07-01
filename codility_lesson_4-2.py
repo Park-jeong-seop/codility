9c6bcc209cf2668bc236f667f18bb96345c56be2
Park-jeong-seop <twinpapa2003@gmail.com>
2018-07-01 23:53:26 +0900

    Update codility_lesson_4-2.py

--- codility_lesson_4-2.py
+++ codility_lesson_4-2.py
@@ -4,9 +4,11 @@ A is List, X is number
 If A contains all the values from 0 to X
 Returns the position value of the last found value
 
+find success :
 if A is [1, 3, 1, 5, 4, 2, 3, 5, 4], X is 5
 return 5
 
+find fail :
 if A is [2, 2, 2], X is 2
 return -1
 """
